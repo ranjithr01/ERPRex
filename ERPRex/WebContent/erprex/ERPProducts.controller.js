96182c5dbe438ead46a61ee4c7eb038d528dba6c
C5203815 <C5203815@INLN50868154A.apj.global.corp.sap>
2015-07-07 09:51:56 +0530

    hide Pusch

--- ERPRex/WebContent/erprex/ERPProducts.controller.js
+++ ERPRex/WebContent/erprex/ERPProducts.controller.js
@@ -23,17 +23,19 @@ sap.ui.getCore().setModel(oModel,"products");
 * @memberOf erprex.ERPProducts
 */
 //	onBeforeRendering: function() {
-//
-//	},
+	//	$("#formId").hide();
+
+	//},
 
 /**
 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 * This hook is the same one that SAPUI5 controls get after being rendered.
 * @memberOf erprex.ERPProducts
 */
-//	onAfterRendering: function() {
-//
-//	},
+	onAfterRendering: function() {
+		$("#formId").hide();
+
+	},
 
 /**
 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
